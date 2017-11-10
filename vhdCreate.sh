3bd63cd194a92ced761300cb2d80e46e2f3caaa7
Magnus Glantz <sudo@redhat.com>
2017-11-10 10:44:55 +0100

    Fixed SSH key

--- vhdCreate.sh
+++ vhdCreate.sh
@@ -4,7 +4,7 @@
 
 # Resource group name
 GROUP=$1
-# VM image file name
+# Local VM image file name
 FILE=$2
 # Storage account name
 ACCOUNT=$3
@@ -37,10 +37,9 @@ read -p "Try to deploy VM from image? (y/n)" ANSWER
 if echo $ANSWER|grep -i "y" >/dev/null; then
   read -p "Enter name of virtual machine: " VMNAME
   az disk create --resource-group $GROUP --name ${VMNAME}ManagedDisk --source https://$ACCOUNT.blob.core.windows.net/vhds/$IMAGENAME
-  az vm create -g $GROUP -l $LOCATION -n $VMNAME --attach-os-disk ${VMNAME}ManagedDisk --os-type linux --admin-username deploy --generate-ssh-keys
-  az vm user update --resource-group $GROUP -n $VMNAME -u deploy --ssh-key-value "$(< ~/.ssh/id_rsa.pub)"
+  az vm create -g $GROUP -l $LOCATION -n $VMNAME --attach-os-disk ${VMNAME}ManagedDisk --os-type linux --admin-username deploy --ssh-key-value ~/.ssh/id_rsa.pub
   IP_ADDRESS=$(az vm list-ip-addresses -g $GROUP -n $VMNAME --query "[0].virtualMachine.network.publicIpAddresses[0].ipAddress" -o tsv)
-  echo "You can now connect using 'ssh deploy@${IP_ADDRESS}'"
+  echo "If your image works, you should be able to connect using 'ssh deploy@${IP_ADDRESS}'"
 else
   echo "Done. Blob URL is: https://$ACCOUNT.blob.core.windows.net/vhds/$IMAGENAME.vhd"
 fi
