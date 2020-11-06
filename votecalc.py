7e9a768a62253dc4e60e65825a6d40f210c7c292
Magnus Glantz <sudo@redhat.com>
2020-11-06 10:07:06 +0100

    Default round to zero
    By default, cast to int (rounding down to zero).
    We're just doing that when printing information, not when calculating result.
    "--roundup false" prints full decimals.
    
    Credits to Morten Jul Jensen ;)

--- votecalc.py
+++ votecalc.py
@@ -7,6 +7,7 @@ import sys
 import os
 import argparse
 import math
+from datetime import datetime
 
 # Argument parsing and --help
 parser = argparse.ArgumentParser(usage='%(prog)s [options]')
@@ -14,15 +15,20 @@ parser.add_argument('--biden', metavar='N', type=int, default=0, help='Number of
 parser.add_argument('--trump', metavar='N', type=int, default=0, help='Number of votes for Trump')
 parser.add_argument('--reported', metavar='N', type=int, default=0, help='Total votes reported')
 parser.add_argument('--percent', metavar='N', type=float, default=0, help='Percent votes reported')
+parser.add_argument('--roundup', metavar='true|false', default='true', help='Round down to zero')
 args = parser.parse_args()
 
 biden = args.biden
 trump = args.trump
+roundup = args.roundup
 reported = args.reported
 percent = args.percent
 vote_per_percent = (reported / percent)
 total_votes = vote_per_percent * 100
 votes_left = total_votes - reported
+
+dateTimeObj = datetime.now()
+print("Running analysis: ", dateTimeObj)
 
 # Yes, below is a simplification, as we are not considering that remaining 
 # votes may be cast on other candidates than Biden or Trump. Doing so introduces a moving target which takes us to
@@ -43,14 +49,28 @@ biden_needs_per = (biden_needs / votes_left)*100
 print("Biden votes: ", biden)
 print("Trump votes: ", trump)
 print("Total votes in: ", reported)
-print("Votes per percent: ", vote_per_percent)
-print("Total votes: ", total_votes)
-print("Votes left to report: ", votes_left)
+if ( roundup == "true" ):
+    print("Votes per percent: ", int(vote_per_percent))
+else:
+    print("Votes per percent: ", vote_per_percent)
+
+if ( roundup == "true" ):
+    print("Total votes: ", int(total_votes))
+else:
+    print("Total votes: ", total_votes)
+
+if ( roundup == "true" ):
+    print("Votes left to report: ", int(votes_left))
+else:
+    print("Votes left to report: ", votes_left)
 
 if ( biden > trump ):
     diff = biden - trump
     print("Biden leads with ", diff, "votes")
-    print("Trump needs number of votes: ", trump_needs)
+    if ( roundup == "true" ):
+        print("Trump needs number of votes: ", int(trump_needs))
+    else:
+        print("Trump needs number of votes: ", trump_needs)
     print("Trump needs % of vote left: ", trump_needs_per)
     if ( votes_left > diff ):
         print("Trump can still win.")
@@ -60,11 +80,12 @@ if ( biden > trump ):
 if ( trump > biden ):
     diff = trump - biden
     print("Trump leads with ", diff, "votes")
-    print("Biden needs number of votes: ", biden_needs)
+    if ( roundup == "true" ):
+        print("Biden needs number of votes: ", int(biden_needs))
+    else:
+        print("Biden needs number of votes: ", biden_needs)
     print("Biden needs % of vote left: ", biden_needs_per)
     if ( votes_left > diff ):
         print("Biden can still win.")
     else:
         print("Trump won the state.")
-
-
